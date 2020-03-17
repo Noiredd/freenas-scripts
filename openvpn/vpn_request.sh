b20d346188e702a031f96aaa5a7a5f715d08325d
Noiredd <snowball91b@gmail.com>
2020-03-17 22:58:06 +0100

    [openvpn] fix: bad name of the logging script as called by vpn_request

--- openvpn/vpn_request.sh
+++ openvpn/vpn_request.sh
@@ -9,7 +9,7 @@ port_request () {
   json=`curl "http://209.222.18.222:2000/\?client_id=$client_id" 2>/dev/null`
   if [ "$json" == "" ]; then
     json='Port forwarding is already activated on this connection, has expired, or you are not connected to a PIA region that supports port forwarding'
-    . $path/vpn_notify.sh $json
+    . $path/vpn_log.sh $json
     exit
   fi
   port=$(echo $json | python2.7 -c 'import sys;exec("j="+sys.stdin.read());print j["port"]')
